2b40a2c47d55566c1dc4fac9da744cdc313927de
AdinGilang <147631071+AdinGilang@users.noreply.github.com>
2023-11-22 21:57:05 +0700

    Add files via upload

--- LaguAnakAyamTurun.java
+++ LaguAnakAyamTurun.java
@@ -10,20 +10,20 @@ public class LaguAnakAyamTurun {
         
         System.out.println("===  Perulangan For ===");
         for (int i = 5; i >= 1; i--) {
-            System.out.println("Anak ayam turun " + i + ", mati satu tinggal " + (i - 1));
+            System.out.println("Tek kotek kotek kotek anak ayam turun  " + i + ", mati satu tinggallah " + (i - 1));
         }
       
         System.out.println("\n===  Perulangan While ===");
         int j = 5;
         while (j >= 1) {
-            System.out.println("Anak ayam turun " + j + ", mati satu tinggal " + (j - 1));
+            System.out.println("Tek kotek kotek kotek anak ayam turun   " + j + ", mati satu tinggallah " + (j - 1));
             j--;
         }
 
         System.out.println("\n===  Perulangan Do-While ===");
         int k = 5;
         do {
-            System.out.println("Anak ayam turun " + k + ", mati satu tinggal " + (k - 1));
+            System.out.println("Tek kotek kotek kotek anak ayam turun  " + k + ", mati satu tinggallah " + (k - 1));
             k--;
         } while (k >= 1);
     }
